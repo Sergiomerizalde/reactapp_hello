9eb85f27b5aba599f4dcde9f58d996530304e125
Sergiomerizalde <sergio.merizalde.p@gmail.com>
2022-08-20 17:16:47 +0000

    final commit

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -4,35 +4,32 @@ import React from "react";
 const Cards = () => {
 	return (
     <div>
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
+        <div className="row col g-4">
+            <div className="card col">
+                <img src="https://img.archiexpo.es/images_ae/photo-g/59228-17634542.webp" classNameName="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                </div>
+            <div className="card col" >
+                <img src="https://img.archiexpo.es/images_ae/photo-g/59228-17634542.webp" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
-            </div>	
+            <div className="card col">
+                <img src="https://img.archiexpo.es/images_ae/photo-g/59228-17634542.webp" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>	
     </div>
 	);
 };
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,25 @@ import React from "react";
 
 //create your first component
 const Navbar = () => {
-	return (<>
-		<nav className="navbar navbar-light bg-light">
-    <div className="container">
-    <a className="navbar-brand" href="#">
-      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24"/>
-    </a>
-    </div>
-    </nav>
-</>
-	);
+    return ( < >
+        <
+        nav className = "navbar navbar-light bg-dark" >
+        <
+        div className = "container" >
+        <
+        a className = "navbar-brand"
+        href = "#" >
+        <
+        img src = "/docs/5.0/assets/brand/bootstrap-logo.svg"
+        alt = ""
+        width = "30"
+        height = "24" / >
+        <
+        /a> <
+        /div> <
+        /nav> <
+        />
+    );
 };
 
 export default Navbar;
